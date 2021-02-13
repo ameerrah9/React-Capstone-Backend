5bd0cfa70943f9516f4f39f5c771d2b770e1e328
Ghameerah McCullers <mccullers.ghameerah@gmail.com>
2021-02-13 16:10:12 -0500

    Adding signup feature

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -19,6 +19,7 @@ class Api::V1::UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user_id
       render json: UserSerializer.new(@user), status: :created
     else
 
