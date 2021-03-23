2eebfca6158bdee8c06111e151394fba834079be
Ghameerah McCullers <mccullers.ghameerah@gmail.com>
2021-03-23 00:36:36 -0400

    added yml

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://game-zone.surge.sh/'
+    origins 'https://gamezone.surge.sh/'
     
     resource '*',
       headers: :any,
