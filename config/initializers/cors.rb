2723116e6e6c64017400bcd79a26349b3ff10660
Ghameerah McCullers <mccullers.ghameerah@gmail.com>
2021-04-08 14:18:27 -0400

    Sort Success

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://localhost3001', 'https://localhost3000'
+    origins 'localhost:3001', 'localhost:3000'
     
     resource '*',
       headers: :any,
